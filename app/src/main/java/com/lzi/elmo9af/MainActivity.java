50b46defef02550d0d829108e05dcfc133fd1b86
LoloziAnas <lolozianas1@gmail.com>
2020-09-04 13:19:14 +0100

    changing drawer layout menu

--- app/src/main/java/com/lzi/elmo9af/MainActivity.java
+++ app/src/main/java/com/lzi/elmo9af/MainActivity.java
@@ -1,20 +1,26 @@
 package com.lzi.elmo9af;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
-import com.lzi.elmo9af.utils.Constants;
+
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
+    private DrawerLayout drawerLayout;
     private ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,43 +29,53 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,
+                R.string.open_navigation_drawer,R.string.close_navigation_drawer);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        // Find the view pager that will allow the user to swipe between fragments
         viewPager = findViewById(R.id.viewpager);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        assert navigationView != null;
+        navigationView.setNavigationItemSelectedListener(this);
+
+        onNavigationItemSelected(navigationView.getMenu().getItem(0).setChecked(true));
+
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)){
+            drawerLayout.closeDrawer(GravityCompat.START);
+        }else
+            super.onBackPressed();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.buttom_navigation_menu,menu);
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.buttom_navigation_menu,menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()){
-
-        }
-        return false;
-    }
-
-    @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-        int id = menuItem.getItemId();
-
-        if (id == R.id.nav_home){
-            viewPager.setCurrentItem(Constants.HOME);
-        }
-        if (id == R.id.nav_account){
-            viewPager.setCurrentItem(Constants.ACCOUNT);
-        }
-        if (id == R.id.nav_paiment){
-            viewPager.setCurrentItem(Constants.PAYMENT);
-        }
-
-
-        return false;
+        return true;
     }
 
-    @Override
-    public void onPointerCaptureChanged(boolean hasCapture) {
 
-    }
 }
