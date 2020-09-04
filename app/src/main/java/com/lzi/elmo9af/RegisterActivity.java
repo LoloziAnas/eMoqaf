65f63a5948d31d99929f9f47f831ec3de72f490a
bermar2011 <bermar2011@gmail.com>
2020-09-04 17:33:40 +0200

    btn connexion rounded

--- app/src/main/java/com/lzi/elmo9af/RegisterActivity.java
+++ app/src/main/java/com/lzi/elmo9af/RegisterActivity.java
@@ -2,7 +2,11 @@ package com.lzi.elmo9af;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +14,6 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
 }
